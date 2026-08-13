7921b717e18363f9f83989fe7399e881c4cdb62e
CowCowFirst <cowcow.first1@gmail.com>
2026-08-13 09:34:34 +0800

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Paste your Supabase Publishable key below.
 // This is intended for browser use. Never put a sb_secret_ key here.
-window.PASSBOOK_SUPABASE_KEY = 'sb_publishable_sb_publishable_lDVhC6_seB2_4FTAwsnr7A_W4ODFN_Y';
+window.PASSBOOK_SUPABASE_KEY = 'sb_publishable_lDVhC6_seB2_4FTAwsnr7A_W4ODFN_Y';
